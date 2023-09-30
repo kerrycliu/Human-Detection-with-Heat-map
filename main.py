ab9adb42895ea58295c8064b30fa7b8ba2fb7386
ErrorUnknown1027 <143456908+ErrorUnknown1027@users.noreply.github.com>
2023-09-30 15:33:06 -0700

    Update main.py
    opens webcam, and takes an image every 5 seconds

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-import numpy as np
 import cv2 as cv
+import numpy as np
+import time
 
-vidCap = cv.VideoCapture(0)
-if not vidCap.isOpened():
-    print("Camera cannot be opened")
-    exit(2)
+vid = cv.VideoCapture(0)  #0 is for the main camera
+if not vid.isOpened():  #check if there is a camera to open
+    print("Can't open camera")
+    exit()
+
+start_time = time.time()#inital start time
+count = 0 #number of images
 while True:
-    ret, frame = vidCap.read()
+    #capture frame by frame
+    ret, frame = vid.read()
 
+    #if frame is read corretly ret is True
     if not ret:
-        print("did not receive frame. closing")
+        print("Can't receive frame (stream end?). Existing...")
         break
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
+    cv.imshow('frame', frame)  #display webcam
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    if elapsed_time >= 5:  #if time passed is 5 seconds take image
+        print("taking image") #comment this out later
+        print(elapsed_time) #comment this out later
+        cv.imwrite("frame%d.jpg" %count, frame)#save image
+        start_time = time.time()  # new start time since its time after image is taken
+        #count += 1 #used to save multiple images
+
+    if cv.waitKey(1) == ord('q'):#press q to turn of webcam and quit the program
         break
 
-vidCap.release()
+#when everything is done release the caputre
+vid.release()
 cv.destroyAllWindows()
-exit(0)
+exit()
